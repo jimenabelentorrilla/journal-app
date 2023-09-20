34b8fdf0136694583a488bc515cd2d62a50c936a
Jimena Belen <jiment4@hotmail.com>
2023-09-20 13:11:28 -0300

    navbar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography> JournalPage</Typography>
+    <JournalLayout>
+      
+      <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet facilis ipsa fugiat laudantium nihil fugit quas quis corrupti neque pariatur quidem corporis omnis eaque molestiae id, voluptatibus sequi? Iure, distinctio!</Typography>
+    
+
+    
+    </JournalLayout>
   )
 }
