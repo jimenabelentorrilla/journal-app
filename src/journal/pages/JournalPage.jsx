7e16ac1fb067dc0b61d76b2f8f30105f13179f31
Jimena Belen <jiment4@hotmail.com>
2023-09-21 15:32:57 -0300

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet facilis ipsa fugiat laudantium nihil fugit quas quis corrupti neque pariatur quidem corporis omnis eaque molestiae id, voluptatibus sequi? Iure, distinctio!</Typography>
+      {/*<Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet facilis ipsa fugiat laudantium nihil fugit quas quis corrupti neque pariatur quidem corporis omnis eaque molestiae id, voluptatibus sequi? Iure, distinctio!</Typography>*/}
     
-
+      <NothingSelectedView />
     
     </JournalLayout>
   )
